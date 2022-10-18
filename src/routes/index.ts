fdf24e333d1b490e97628f7e69bea2b473e91a42
HERICK CEZAR DIAS ANDRADE <herick.cdandrade@senacsp.edu.br>
2022-10-18 10:20:15 -0300

    first commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,52 +1,112 @@
-import {Router, Request, Response} from "express"
+import { Router, Request, Response } from "express"
+
 const router = Router()
 
+router.get('/',(req:Request,res:Response) =>{
 
-router.get('/',(req: Request,res: Response) =>{
-    res.render('home')
+   let user:string = 'Viviane'
+
+    res.render('pages/home',{
+       user,
+       produtos:[
+        {titulo:'Livro Crepusculo',preco:39},
+        {titulo:'Arroz São João', preco: 15},
+        {titulo:'Bombril', preco:5},
+        {titulo:'Salgadinho Fofura',preco:3}
+       ],frases:[
+        'A guerra contra as drogas é inutil pois acaba morrendo os dois lados',
+        'A Vingança é plena mata alma e envenena',
+        'Prefiro morrer do que perder a vida',
+        'A vida passa mais rápido do que uma Ferrari na estrada'
+       ]
+
+    })
 })
 
-router.get('/login',(req: Request,res: Response) =>{
-    res.render('login')
+router.get('user',(req:Request,res:Response) =>{
+    res.render('user',{
+        name:'Fulano',
+        lastName:'Fulaninho',
+        showWelcome:false
+    })
 })
 
-router.get('/noticia/:politica',(req: Request,res: Response) =>{
-    let politica: string = req.params.politica
-    res.send("Noticias: "+politica)
+router.get('/idade',(req:Request,res:Response) =>{
+    res.render('pages/idade')
 })
 
-router.get('/esportes/:futebol', (req: Request,res: Response) =>{
-    let futebol: string = req.params.futebol
-    res.send("Esportes: "+futebol)
+router.post('/idade-resultado',(req:Request, res:Response) =>{
+    let mostrarIdade: boolean = false
+    let idade: number = 0
+
+    if(req.query.anoNascimento){
+        let anoNascimento:number = parseInt(req.query.anoNascimento as string)
+        let anoAtual: number = new Date().getFullYear()
+
+        idade= anoAtual - anoNascimento
+
+        mostrarIdade = true
+    }
+
+    res.render('pages/idade',{
+        mostrarIdade,
+        idade
+    })
+
 })
 
-router.get('/viagens/:origem-:destino',(req: Request,res: Response) =>{
+
+router.get('/login',(req:Request,res:Response) =>{
+    res.render('pages/login')
+})
+
+router.post('/login-usuario',(req:Request, res:Response) =>{
+    let email: string = req.body.email as string
+    let senha: string = req.body.senha as string
+
+
+    res.render('pages/login',{
+        email,
+        senha
+    })
+})
+
+ 
+
+
+
+
+//ROTA ESTÁTICA
+router.get('/contato',(req:Request,res:Response) =>{
+    res.send("PAGINA DE CONTATO")
+})
+//ROTA DINÂMICA
+router.get('/noticia/:noticia',(req:Request,res:Response) =>{
+    let noticia: string = req.params.noticia
+    res.send(`Noticias: ${noticia}`)
+})
+
+router.get('/viagens/:origem-:destino',(req:Request,res:Response) =>{
     let origem:string = req.params.origem
     let destino:string = req.params.destino
 
     res.send(`Procurando trajetos de ${origem} até ${destino}`)
 })
 
-/*import filmes from './filmes'
-import Filmes from './filmes'
+router.get('/nome',(req:Request, res:Response) =>{
+    let nome: string = req.query.nome as string
+    let idade: number = parseInt(req.query.idade as string)
+    let telefone: number = parseInt(req.query.telefone as string)
+    let endereco: string = req.query.endereco as string
+    let informacoes: boolean = true
 
-console.log(filmes)
-
-/*import Matematica from './matematica'
-let n1: number = 10
-let n2: number = 20
-console.log("SOMA: "+Matematica.somar(n1,n2))
-console.log("SUBTRAIR: "+Matematica.subtrair(n1,n2))
-console.log("MULTIPLICAR: "+Matematica.multiplicar(n1,n2))
-
-/*import validator from "validator"
-let niver = '2004-07-01'
-console.log(validator.isDate(niver))
-
-/*console.log(validator.isIP(ip))
-
-console.log(validator.isEmail("herickczr07@gmail.com"))
-
-console.log ("Olá mundo!")*/
+    res.render('pages/nome',{
+        nome,
+        idade,
+        telefone,
+        endereco,
+        informacoes
+    })
+})
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,32 @@
-import express ,{Request, Response} from 'express'
+import express,{Request, Response} from 'express'
 import mainRoutes from './routes/index'
-
 import path from 'path'
-
+//importando o mustache para usar o HTML
 import mustache from 'mustache-express'
 
+//USANDO EXPRESS
 const server = express()
-
+//configurando o mustache
 server.set('view engine','mustache')
-
+//criando uma rota para a pasta public
 server.use(express.static(path.join(__dirname,'../public')))
-
+//criando a rota para a pasta views
 server.set('views',path.join(__dirname,'views'))
 server.engine('mustache',mustache())
 
+//HABILITANDO O POST
+server.use(express.urlencoded({extended:true}))
 
 server.use(mainRoutes)
-
-server.use((req:Request, res:Response) =>{
+//criando a página não encontrada
+server.use( (req:Request, res:Response) =>{
     res.status(404).send("Página não encontrada")
 })
+server.listen(4000)
 
-server.listen(3000)
+
+
+
 
 
 
